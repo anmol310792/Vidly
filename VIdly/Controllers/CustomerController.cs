69438eeb38ec2de9c2d3dafd75e6268cf54ba7eb
anmol310792 <38555380+anmol310792@users.noreply.github.com>
2019-07-13 09:35:05 +0530

    Customer setups

---
+++ VIdly/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VIdly.Models;
+
+namespace VIdly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customer = new Customer()
+            {
+                Name = "Pragya"
+            };
+            return View(customer);
+        }
+    }
+}
