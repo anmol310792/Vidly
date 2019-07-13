bdf60cc00ffd9bdb7759dced6cc44e7960a7ab69
anmol310792 <38555380+anmol310792@users.noreply.github.com>
2019-07-13 08:57:36 +0530

    Theme changes

--- VIdly/App_Start/BundleConfig.cs
+++ VIdly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace VIdly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-Cerulean.css",
                       "~/Content/site.css"));
         }
     }
